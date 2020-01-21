3c264222a53c8ded902365d1f5964912c95c4977
Tiago Pereira <tiago.afper@gmail.com>
2020-01-21 10:34:53 +0000

    Externalizaçao do codigo da Class Task

--- Task.php
+++ Task.php
@@ -1,7 +1,23 @@
 <?php
 
-
+require_once "baseDados.php";
 class Task
 {
+    private $mAutor, $dataInicio, $mDataFim, $mDuracao;
 
+    public function calculaDuracao($pTaskID)
+    {
+
+        $bd = new baseDados();
+        $bd->selecionarTarefaOnde($pTaskID);
+
+        $row = mysqli_fetch_array($bd->selecionarTarefaOnde($pTaskID));
+        $mDataInicio = $row['dataInicio'];
+
+        $dataFim = date_create(date("Y/m/d"));
+        $dataInicio = date_create($mDataInicio);
+        $mDuracao = date_diff($dataInicio, $dataFim);
+
+        return $mDuracao->format('%a');
+    }
 }
